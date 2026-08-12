d42d6e7e5c0b401f899c91cbe23f0b4c1eebcd14
dhiyasalmas <dhiyasalmas@gmail.com>
2026-08-12 16:42:06 +0700

    Add recommendation engine

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
-# Tugas 1, Dhiya Salma Salsabila
+# Tugas 2, Dhiya Salma Salsabila
+from services.trip_service import calculate_budget, get_trip_category, recomendation_destination, get_travel_season
 
 def print_trip_summary():
-    destination = input("Destination : ")
-    country = input("Country : ")
+    #destination = input("Destination : ")
+    country = input("Choose country (japan, amerika, indonesia) : ")
     days = int(input("Days : "))
     budget = int(input("Budget : "))
     currency = input("Currency : ")
@@ -11,24 +12,35 @@ def print_trip_summary():
     food_cost =  float(input("Food Cost per Day: "))
     micellaneous_cost =  float(input("Micellaneous Cost : "))
     travel_month = input("Travel Month : ")
-    total_estimated_cost = ((hotel_cost + transportation_cost + food_cost)*days)+micellaneous_cost
+    #total_estimated_cost = ((hotel_cost + transportation_cost + food_cost)*days)+micellaneous_cost
+    budget_perday, total_estimated_cost, rest_budget = calculate_budget(days, budget, hotel_cost, transportation_cost, food_cost)
     print("=================================")
     print("KelanaAi")
     print("=================================")
-    print(f"Destination : {destination}")
+    #print(f"Destination : {destination}")
     print(f"Country :  {country}")
     print(f"Days : {days}")
     print(f"Budget : {budget}")
     print(f"Currency : {currency}")
-    print(f"Hotel Cost : {hotel_cost}")
-    print(f"Transportation cost : {transportation_cost}")
-    print(f"Food Cost : {food_cost}")
+    print(f"Hotel Cost Per Day : {hotel_cost}")
+    print(f"Transportation cost Per Day : {transportation_cost}")
+    print(f"Food Cost Per Day : {food_cost}")
     print(f"Micellaneous Cost : {micellaneous_cost}")
     print(f"Travel Month : {travel_month}")
-    if budget < total_estimated_cost:
+    season = get_travel_season(travel_month)
+    print(f"Season : {season}")
+    print(f"Budget Per Day : {budget_perday}")
+    print(f"Total Estimated Cost Per Day : {total_estimated_cost}")
+    print(f"Rest of Money : {rest_budget-micellaneous_cost}")
+    category, transportation = get_trip_category(budget)
+    print(f"Travel Category : {category}")
+    print(f"Transportation : {transportation}")
+    recomendation_destination(country)
+    if rest_budget < 0:
         print ("Budget Exceeded!")
     else:
         print("Have a good travel!")
+    
 
 if __name__ == "__main__":
     print_trip_summary()
---
+++ backend/services/trip_service.py
@@ -0,0 +1,36 @@
+def calculate_budget(days, budget, hotel_cost, transportation_cost, food_cost):
+    budget_perday = budget/days
+    total_estimated_cost = ((hotel_cost + transportation_cost + food_cost)) # Cost per day
+    rest_budget = budget - (total_estimated_cost*days)
+    return budget_perday, total_estimated_cost, rest_budget
+
+def get_trip_category(budget):
+    if budget < 1000:
+        return "Backpacker", "Bus"
+    elif budget <= 3000:
+        return "Standard", "Train"
+    else:
+        return "Luxury", "Flight"
+
+def recomendation_destination(country):
+    japan = ["tokyo", "shibuya", "akihabara", "kyoto", "osaka"]
+    amerika = ["new york", "los angles", "chicago", "michigan", "oregon"]
+    indonesia = ["bandung", "jakarta", "semarang", "malang", "yogyakarta"]
+    print("Recommended Places")
+    if country == "japan":
+        for place in japan:
+            print(f"- {place}")
+    elif country == "amerika":
+        for place in amerika:
+            print(f"- {place}")
+    else:
+        for place in indonesia:
+            print(f"- {place}")
+
+def get_travel_season(travel_month):
+    if travel_month == "December":
+        return("Peak Season")
+    elif travel_month == "June":
+        return("Holiday Season")
+    else:
+        return("Regular Season")
