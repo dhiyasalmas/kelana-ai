dd1f7acfa5b81e3df5c5688f4e209426346a8d89
dhiyasalmas <dhiyasalmas@gmail.com>
2026-08-14 16:51:16 +0700

    Convert KelanaAI into FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,46 +1,63 @@
-# Tugas 2, Dhiya Salma Salsabila
-from services.trip_service import calculate_budget, get_trip_category, recomendation_destination, get_travel_season
+from fastapi import FastAPI
+from pydantic import BaseModel
+from services.trip_service import (
+    calculate_budget,
+    get_trip_category,
+    get_travel_season
+)
 
-def print_trip_summary():
-    #destination = input("Destination : ")
-    country = input("Choose country (japan, amerika, indonesia) : ")
-    days = int(input("Days : "))
-    budget = int(input("Budget : "))
-    currency = input("Currency : ")
-    hotel_cost = float(input("Hotel Cost per Day : "))
-    transportation_cost = float(input("Transportation cost per Day : "))
-    food_cost =  float(input("Food Cost per Day: "))
-    micellaneous_cost =  float(input("Micellaneous Cost : "))
-    travel_month = input("Travel Month : ")
-    #total_estimated_cost = ((hotel_cost + transportation_cost + food_cost)*days)+micellaneous_cost
-    budget_perday, total_estimated_cost, rest_budget = calculate_budget(days, budget, hotel_cost, transportation_cost, food_cost)
-    print("=================================")
-    print("KelanaAi")
-    print("=================================")
-    #print(f"Destination : {destination}")
-    print(f"Country :  {country}")
-    print(f"Days : {days}")
-    print(f"Budget : {budget}")
-    print(f"Currency : {currency}")
-    print(f"Hotel Cost Per Day : {hotel_cost}")
-    print(f"Transportation cost Per Day : {transportation_cost}")
-    print(f"Food Cost Per Day : {food_cost}")
-    print(f"Micellaneous Cost : {micellaneous_cost}")
-    print(f"Travel Month : {travel_month}")
-    season = get_travel_season(travel_month)
-    print(f"Season : {season}")
-    print(f"Budget Per Day : {budget_perday}")
-    print(f"Total Estimated Cost Per Day : {total_estimated_cost}")
-    print(f"Rest of Money : {rest_budget-micellaneous_cost}")
-    category, transportation = get_trip_category(budget)
-    print(f"Travel Category : {category}")
-    print(f"Transportation : {transportation}")
-    recomendation_destination(country)
-    if rest_budget < 0:
-        print ("Budget Exceeded!")
-    else:
-        print("Have a good travel!")
-    
 
-if __name__ == "__main__":
-    print_trip_summary()
+class TripRequest(BaseModel):
+    destination: str
+    days: int
+    budget: float
+    hotel_cost: float
+    transportation_cost: float
+    food_cost: float
+    travel_month: str 
+
+app = FastAPI()
+
+# GET endpoint
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to KelanaAI"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "OK"
+    }
+
+# POST
+@app.post("/api/v1/trips")
+def create_trip(request: TripRequest):
+    budget_perday, total_estimated_cost, rest_budget = calculate_budget(
+        request.days,
+        request.budget,
+        request.hotel_cost,
+        request.transportation_cost,
+        request.food_cost
+    )
+
+    category, transport = get_trip_category(
+        request.budget
+    )
+
+    season = get_travel_season(
+        request.travel_month
+    )
+
+    return {
+        "destination": request.destination,
+        "days": request.days,
+        "budget": request.budget,
+        "budget_perday": budget_perday,
+        "total_estimated_cost": total_estimated_cost,
+        "rest_budget": rest_budget,
+        "category": category,
+        "recommended_transport": transport,
+        "travel_season": season
+    }
